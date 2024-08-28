cd5deff73ed6b4ae02e6f3b17b2a062263002676
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-08-28 01:56:55 -0300

    feat: first component and page setting

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,24 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface SectionSectionMetadata extends Schema.Component {
+  collectionName: 'components_section_section_metadata';
+  info: {
+    displayName: 'section_metadata';
+    icon: 'apps';
+  };
+  attributes: {
+    name: Attribute.String & Attribute.Required;
+    section_id: Attribute.String & Attribute.Required & Attribute.Unique;
+    background: Attribute.Boolean &
+      Attribute.Required &
+      Attribute.DefaultTo<false>;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'section.section-metadata': SectionSectionMetadata;
+    }
+  }
 }
